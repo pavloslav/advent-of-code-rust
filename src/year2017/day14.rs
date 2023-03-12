d069252edc18a0ac4494c3bc55d4f82c7955c889
Pavloslav <pavloslav@ukr.net>
2023-03-12 22:57:24 +0200

    2017-14-2

--- src/year2017/day14.rs
+++ src/year2017/day14.rs
@@ -15,6 +15,52 @@ pub fn task1(input: &str) -> usize {
         .sum()
 }
 
+fn find_start(map: &[Vec<bool>]) -> Option<(usize, usize)> {
+    for x in 0..map.len() {
+        for y in 0..map[x].len() {
+            if map[x][y] {
+                return Some((x, y));
+            }
+        }
+    }
+    None
+}
+
+fn floodfill(map: &mut [Vec<bool>], (x, y): (usize, usize)) {
+    if map[x][y] {
+        map[x][y] = false;
+        if x > 0 {
+            floodfill(map, (x - 1, y))
+        }
+        if y > 0 {
+            floodfill(map, (x, y - 1))
+        }
+        if x < map.len() - 1 {
+            floodfill(map, (x + 1, y))
+        }
+        if y < map.len() - 1 {
+            floodfill(map, (x, y + 1))
+        }
+    }
+}
+
 pub fn task2(input: &str) -> usize {
-    unimplemented!();
+    let mut map: Vec<Vec<bool>> = (0..128)
+        .map(|i| {
+            super::knots_hash::dense_hash(
+                format!("{input}-{i}").bytes().map(|c| c.into()),
+            )
+            .iter()
+            .flat_map(|&x| (0..8).map(move |i| (x >> (7 - i)) & 1 == 1))
+            .collect()
+        })
+        .collect();
+    for i in 0.. {
+        if let Some(pos) = find_start(&map) {
+            floodfill(&mut map, pos);
+        } else {
+            return i;
+        }
+    }
+    unreachable!()
 }
