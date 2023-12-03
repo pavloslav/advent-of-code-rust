8fb91719872632e2d236096b6042f0272f3f8b6c
Pavloslav <pavloslav@ukr.net>
2023-12-03 08:26:41 +0200

    2023-3

---
+++ year2023/src/day3.rs
@@ -0,0 +1,84 @@
+use crate::*;
+
+pub fn parse_input(input: &str) -> Result<Vec<&str>> {
+    Ok(input.lines().collect())
+}
+
+pub fn task1(input: &[&str]) -> Result<usize> {
+    let mut sum = 0;
+
+    for (y, &line) in input.iter().enumerate() {
+        let mut x = line
+            .find(|c: char| c.is_ascii_digit())
+            .unwrap_or(line.len());
+        while x < line.len() {
+            let x_last = x + line[x..]
+                .find(|c: char| !c.is_ascii_digit())
+                .unwrap_or(line[x..].len());
+            let left = x.saturating_sub(1);
+            let right = (x_last + 1).min(line.len());
+            let top = y.saturating_sub(1);
+            let bottom = (y + 2).min(input.len());
+            let has_symbol = input[top..bottom]
+                .iter()
+                .flat_map(|l| l[left..right].chars().to_owned())
+                .any(|c| c != '.' && !c.is_ascii_digit());
+            if has_symbol {
+                sum += line[x..x_last].parse::<usize>().map_err(|e| {
+                    aoc_error!("Can't parse the number '{}': {}", &line[x..x_last], e)
+                })?;
+            }
+            x = x_last
+                + line[x_last..]
+                    .find(|c: char| c.is_ascii_digit())
+                    .unwrap_or(line[x_last..].len());
+        }
+    }
+    Ok(sum)
+}
+
+pub fn task2(input: &[&str]) -> Result<usize> {
+    let mut gears = std::collections::HashMap::new();
+
+    for (y, &line) in input.iter().enumerate() {
+        let mut x = line
+            .find(|c: char| c.is_ascii_digit())
+            .unwrap_or(line.len());
+        while x < line.len() {
+            let x_last = x + line[x..]
+                .find(|c: char| !c.is_ascii_digit())
+                .unwrap_or(line[x..].len());
+            let left = x.saturating_sub(1);
+            let right = (x_last + 1).min(line.len());
+            let top = y.saturating_sub(1);
+            let bottom = (y + 2).min(input.len());
+            let value = line[x..x_last]
+                .parse::<usize>()
+                .map_err(|e| aoc_error!("Can't parse the number '{}': {}", &line[x..x_last], e))?;
+            for (dy, line) in input[top..bottom].iter().enumerate() {
+                for (dx, c) in line[left..right].chars().enumerate() {
+                    if c == '*' {
+                        gears
+                            .entry((left + dx, top + dy))
+                            .and_modify(|v: &mut Vec<_>| v.push(value))
+                            .or_insert_with(|| vec![value]);
+                    }
+                }
+            }
+            x = x_last
+                + line[x_last..]
+                    .find(|c: char| c.is_ascii_digit())
+                    .unwrap_or(line[x_last..].len());
+        }
+    }
+    Ok(gears
+        .values()
+        .filter_map(|g| {
+            if g.len() == 2 {
+                Some(g[0] * g[1])
+            } else {
+                None
+            }
+        })
+        .sum())
+}
--- year2023/src/lib.rs
+++ year2023/src/lib.rs
@@ -2,7 +2,7 @@
 common::mod_list!(year2023
     , day1
     , day2 
-    //, day3 
+    , day3 
     //, day4 
     //, day5 
     //, day6 
